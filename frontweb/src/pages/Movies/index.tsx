227d520b245d0a7f3cef2397659de87bb2b46d4d
helderargomes <helderargomes@gmail.com>
2025-03-10 06:11:41 -0300

    Movie Page responsive

--- frontweb/src/pages/Movies/index.tsx
+++ frontweb/src/pages/Movies/index.tsx
@@ -1,11 +1,47 @@
-import { Link } from "react-router-dom";
 import "./styles.css";
 import MovieCard from "components/MovieCard";
 
 const Movies = () => {
   return (
     <div className="home-container movie-page-container">
-      <MovieCard />
+        <div className="row">
+          <div className="col-sm-6 col-xl-3 movie-card-container">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-xl-3 movie-card-container">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-xl-3 movie-card-container">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-xl-3 movie-card-container">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-xl-3 movie-card-container">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-xl-3 movie-card-container">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-xl-3 movie-card-container">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-xl-3 movie-card-container">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-xl-3 movie-card-container">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-xl-3 movie-card-container">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-xl-3 movie-card-container">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-xl-3 movie-card-container">
+            <MovieCard />
+          </div>
+        </div>
     </div>
   );
 };
