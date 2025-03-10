e4a4645628150ba74d16a9eae8f91d0950fc1804
helderargomes <helderargomes@gmail.com>
2025-03-10 18:25:45 -0300

    CategoryFilter and Pagination mobile

---
+++ frontweb/src/components/Pagination/index.tsx
@@ -0,0 +1,18 @@
+import { ReactComponent as ArrowIcon } from "assets/images/arrow-icon.svg";
+import "./styles.css"
+
+const Pagination = () => {
+  return (
+    <div className="pagination-container">
+      <ArrowIcon className="arrow-inactive"/>
+      <div className="pagination-item active">1</div>
+      <div className="pagination-item">2</div>
+      <div className="pagination-item">3</div>
+      <div className="pagination-item">...</div>
+      <div className="pagination-item">5</div>
+      <ArrowIcon className="arrow-active arrow-next"/>
+    </div>
+  );
+};
+
+export default Pagination;
--- frontweb/src/pages/Movies/index.tsx
+++ frontweb/src/pages/Movies/index.tsx
@@ -1,9 +1,15 @@
+import Pagination from "components/Pagination";
 import "./styles.css";
 import MovieCard from "components/MovieCard";
+import MovieCategoryFilter from "components/MovieFilter";
 
 const Movies = () => {
   return (
-    <div className="home-container movie-page-container">
+    <div className="home-base-container movie-page-container">
+        <div className="movie-filter-container">
+          <MovieCategoryFilter />
+        </div>
+        
         <div className="row">
           <div className="col-sm-6 col-xl-3 movie-card-container">
             <MovieCard />
@@ -41,6 +47,10 @@ const Movies = () => {
           <div className="col-sm-6 col-xl-3 movie-card-container">
             <MovieCard />
           </div>
+        </div>
+
+        <div className="row movie-pagination-container">
+          <Pagination />
         </div>
     </div>
   );
